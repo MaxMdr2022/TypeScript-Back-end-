db1c8810c1ff00805d6b57cffa19e860956e734e
MaxMdr2022 <maxi_meder94@hotmail.com>
2023-04-09 22:45:19 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import express from "express"
 
 import diaryRoutes from "./routes/diaries"
 
+// instalamos las cors para poder usar este back como API para el proyecto de front-end con TS subscriptores. 
+// Al instalarlo tiro un error donde decia que faltaban los type de cors y que se pruebe con hacer npm i --save-dev @types/cors
+import cors from "cors"  
 
 const app = express();
 
 app.use(express.json())  // middleware que transforma la req.body en json
 
-const PORT = 3000
+const PORT = 3001
 
 
 // 'req' is declared but its value is never read. Esto es por el "noUnusedParameters". Entonces el "_" sirver para ignorar el parametro que no va a usar. 
@@ -18,6 +21,7 @@ app.get("/", (_req, res) =>{
 });
 
 
+app.use(cors())
 app.use("/api/diaries", diaryRoutes)
 
 
